0badd4cc9fec22b8a640faa15ed173484b5b3769
Your NameKalebWorkneh <kalebazenawi@gmail.com>
2019-06-12 01:34:57 -0500

    Initialize Workspace

---
+++ eCarRent/src/main/java/edu/mum/cs/cs425/project/ecarrent/ECarRentApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.project.ecarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECarRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECarRentApplication.class, args);
+	}
+
+}
